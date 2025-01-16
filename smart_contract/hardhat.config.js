3de7d5e1ab356a7fa4bcd5639b453b1989b2dc1d
Satyam Kumar Sharma <satyamkumarsharma849@gmail.com>
2025-01-16 22:50:47 +0530

    .

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     Sepolia: {
-      url: 'https://eth-mainnet.g.alchemy.com/v2/CVYXtWAhHS01FvZXEAi8lYdg-AitebUz',
+      url: 'https://thirdweb.com/team/8bcacc688f44fc3c88061f4db7a1014456837659/Krypt-project-19af2a',
       accounts: ['fe16d5f501b0a932d6811a067b5a6d2f0f1576d54a13d7453f89f2670dfa4393'],
     },
   },
